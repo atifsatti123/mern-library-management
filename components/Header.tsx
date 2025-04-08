17ae6d676e98664a230380594ec7882a23006888
atif-maxgo123 <atif.maxgo@gmail.com>
2025-04-08 18:24:01 +0500

    complete home page

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,10 @@
 "use client";
+import React from "react";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 
 import { usePathname } from "next/navigation";
-import React from "react";
 
 const Header = () => {
   const pathname = usePathname();
