b6f2a74836ac6810369cd2d7123acf5a01f105e8
atif-maxgo123 <atif.maxgo@gmail.com>
2025-04-14 12:51:27 +0500

    Setup Upstash Redis

--- lib/actions/auth.ts
+++ lib/actions/auth.ts
@@ -4,11 +4,20 @@ import { users } from "@/database/schema";
 import { eq } from "drizzle-orm";
 import { hash } from "bcryptjs";
 import { signIn } from "@/auth";
+import { headers } from "next/headers";
+import ratelimit from "../ratelimit";
+import { redirect } from "next/navigation";
 
 export const signInWithCredentials = async (
   params: Pick<AuthCredentials, "email" | "password">
 ) => {
   const { email, password } = params;
+
+  const ip = (await headers()).get("x-forwarded-for") || "127.0.0.1";
+  const { success } = await ratelimit.limit(ip);
+  
+  if (!success) return redirect("/too-fast");
+
   try {
     const result = await signIn("credentials", {
       email,
@@ -29,6 +38,9 @@ export const signInWithCredentials = async (
 
 export const signup = async (params: AuthCredentials) => {
   const { fullName, email, universityId, password, universityCard } = params;
+  const ip = (await headers()).get("x-forwarded-for") || "127.0.0.1";
+  const { success } = await ratelimit.limit(ip);
+  if (!success) return redirect("/too-fast");
 
   const existingUser = await db
     .select()
