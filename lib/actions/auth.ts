b7d139e6bce1d57a42e275cb5e2d15e26fced2df
atif-maxgo123 <atif.maxgo@gmail.com>
2025-04-16 10:56:14 +0500

    db typeof

--- lib/actions/auth.ts
+++ lib/actions/auth.ts
@@ -1,5 +1,5 @@
 "use server";
-import { db } from "./../../database/drizzle";
+import { db } from "@/database/drizzle";
 import { users } from "@/database/schema";
 import { eq } from "drizzle-orm";
 import { hash } from "bcryptjs";
